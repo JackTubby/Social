035443c9823279a125d78bd1e14690462bb49fd3
JackTubby <jack200034@hotmail.com>
2023-02-10 23:28:48 +0000

    Add bottom nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import BottomNavbar from "../components/BottomnNavbar";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <BottomNavbar />
       <Toaster />
     </>
   );
