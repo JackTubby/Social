baaf8b588735991da859858da9b3b1adf6fcd81f
JackTubby <jack200034@hotmail.com>
2023-02-10 23:29:13 +0000

    Remove temporary code

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,7 @@ import toast from "react-hot-toast";
 export default function Home() {
   return ( 
   <>
-    <Link prefetch={false} href={{
-      pathname: '/[username]',
-      query: {username: 'Jack_000'},
-    }}>Jacks Profile</Link>
-    <Loader show={false}></Loader>
+
   </>
   );
 }
