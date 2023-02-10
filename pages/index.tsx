61bcc14d7f9f2f8c22ef444a54d81b59eb521833
JackTubby <jack200034@hotmail.com>
2023-02-10 22:23:23 +0000

    Import toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Loader from "../components/Loader"
+import toast from "react-hot-toast";
 export default function Home() {
   return ( 
   <>
