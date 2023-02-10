0d09540f746a4a373f77601a917b001abc12556c
JackTubby <jack200034@hotmail.com>
2023-02-10 22:50:20 +0000

    Init

---
+++ components/BottomnNavbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from "next/image";
+
+// Bottom nav
+const BottomNavbar = () => {
+
+  return (
+    <nav aria-label="bottom navbar">
+        
+    </nav>
+  );
+}
+export default BottomNavbar;
