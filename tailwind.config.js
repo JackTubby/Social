d865603d940bb7fc0faf9849ed57ecf36aaaec0c
Jack <jack200034@hotmail.com>
2023-02-13 20:20:17 +0000

    Add colours to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+          colors: {
+            white: "#F2F2F2",
+            black: "#1C1C1C",
+            darkGrey: "#232222",
+            meidumGrey: "#3E3F3E",
+            lightGrey: "#4C4C4C",
+            purple: "#9E6DF1",
+            pink: "#832E4B",
+    },
   },
   plugins: [],
 }
