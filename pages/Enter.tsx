a474a914fa835525423c1a061ec177ada49c29cd
JackTubby <jack200034@hotmail.com>
2023-02-15 17:34:10 +0000

    Create Sign-in/out and userform components & check what to show

--- pages/Enter.tsx
+++ pages/Enter.tsx
@@ -1,7 +1,43 @@
-const EnterPage = () => {
-    return (
-        <main>EnterPage</main>
-    )
-}
+import { auth, googleAuthProvider } from "@/lib/firebase";
 
+const EnterPage = () => {
+  // We need to know the users auth context to know what to show
+  const user = null;
+  const username = null;
+  // User can be in three different states
+  // 1 user signed out <SignInButton />
+  // 2 user signed in, but missing username <UsernameForm />
+  // 3 user signed in, has username <SignOutButton/ >
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+};
 export default EnterPage;
+
+// Sign in with Google btn
+const SignInButton = () => {
+    return (
+        <>
+        <button className="text-white m-20 bg-purple p-4" onClick={signInWithGoogle}>Sign In with google</button>
+        </>
+    );
+};
+
+// Sign out btn
+const SignOutButton = () => {
+    // Removes the JSON web token
+    return <button className="text-white" onClick={() => auth.signOut()}>Sign Out</button>
+};
+
+// Username form for user to select username
+const UsernameForm = () => {};
