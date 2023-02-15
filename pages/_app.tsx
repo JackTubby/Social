02b1fc28c43f84d6a25ca4a7f17b3c12187ce754
JackTubby <jack200034@hotmail.com>
2023-02-15 18:00:16 +0000

    Import and wrap components in UserContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import BottomNavbar from "../components/BottomnNavbar";
 import { Toaster } from "react-hot-toast";
+import { UserContext } from "@/lib/context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <UserContext.Provider value={{user:{}, username: null}}>
       <Navbar />
       <Component {...pageProps} />
       <BottomNavbar />
       <Toaster />
+      </UserContext.Provider>
     </>
   );
 }
