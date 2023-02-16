3b8428965eebb17a2c5eef250727b1931bd14d1b
JackTubby <jack200034@hotmail.com>
2023-02-16 19:03:54 +0000

    Import useUserData hook and provide it to context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,18 @@ import BottomNavbar from "../components/BottomnNavbar";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "@/lib/context";
 
+import { useUserData } from "@/lib/hooks";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const userData = useUserData();
+
   return (
     <>
-    <UserContext.Provider value={{user:{}, username: null}}>
-      <Navbar />
-      <Component {...pageProps} />
-      <BottomNavbar />
-      <Toaster />
+      <UserContext.Provider value={userData}>
+        <Navbar />
+        <Component {...pageProps} />
+        <BottomNavbar />
+        <Toaster />
       </UserContext.Provider>
     </>
   );
