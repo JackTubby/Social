5334d6e8973da690b03ecb3ba209bdabfae54d63
JackTubby <jack200034@hotmail.com>
2023-02-16 23:39:26 +0000

    Init & import components

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,13 @@
-const UserProfilePage = () => {
-    return (
-        <main>UserProfilePage</main>
-    )
-}
+import PostFeed from "@/components/PostFeed";
+import UserProfile from "@/components/UserProfile";
+
+const UserProfilePage = ({ user, posts }) => {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed user={posts} />
+    </main>
+  );
+};
 
 export default UserProfilePage;
