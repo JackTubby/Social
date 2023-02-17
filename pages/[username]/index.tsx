ab094b54b815651b48d8f9a6339a7507b14324e7
JackTubby <jack200034@hotmail.com>
2023-02-17 17:54:22 +0000

    Get data from db to be passed as props

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,13 +1,46 @@
 import PostFeed from "@/components/PostFeed";
 import UserProfile from "@/components/UserProfile";
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
+import { getUserWithUsername, postToJSON } from "@/lib/firebase";
+import { GetServerSideProps } from "next";
 
-const UserProfilePage = ({ user, posts }) => {
-  return (
-    <main>
-      <UserProfile user={user} />
-      <PostFeed user={posts} />
-    </main>
-  );
-};
+export const getServerSideProps: GetServerSideProps = async ({ query: urlQuery }: { query: any }) => {
+    const { username } = urlQuery;
+    // Custom helper func
+    const userDoc = await getUserWithUsername(username);
+    if (!userDoc) {
+        return {
+          notFound: true,
+        };
+      }
+    // JSON serializable data
+    let user = null;
+    let posts = null;
+  
+    if (userDoc) {
+      user = userDoc.data();
+  
+      const postsQuery = query(
+        collection(getFirestore(), userDoc.ref.path, 'posts'),
+        where('published', '==', true),
+        orderBy('createdAt', 'desc'),
+        limit(5)
+      );
+      posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  
+      console.log('how many times is this called?');
+  console.log("1",posts)
+    }
 
-export default UserProfilePage;
+    return {
+        props: { user, posts }, // will be passed to the page component as props
+    };
+  }
+  export default function UserProfilePage({ user, posts }: { user: any, posts: any[] }): JSX.Element {
+    return (
+      <main>
+        <UserProfile user={user} />
+        <PostFeed posts={posts} />
+      </main>
+    );
+  }
