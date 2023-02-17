75fd3008b11f70835b8e45d5f2993897ef05e3c2
JackTubby <jack200034@hotmail.com>
2023-02-17 17:52:23 +0000

    Create feed for posts to be individualy rendered

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,6 +1,34 @@
-const PostFeed = () => {
+import Link from "next/link";
+
+// The admin prop is a boolean that if true provides extra ability for the admin (crud)
+const PostFeed = ({ posts, admin }:any) => {
+    // List of posts that is mapped to an individual post item component
+    return posts ? posts.map((post:any) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
+}
+
+function PostItem({ post, admin }: { post: any, admin: boolean }) {
+    // Method to calc word count and read time
+    const wordCount = post?.content.trim().split(/\s+/g).length;
+    const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+
     return (
-        <main>PostFeed</main>
+        <div>
+            <Link>
+                <a href={`/${post.username}`}>
+                    <strong>By @{post.username}</strong>
+                </a>
+            </Link>
+            <Link href={`/${post.username}/${post.slug}`}>
+                <h2>
+                    <a>{post.title}</a>
+                </h2>
+            </Link>
+            <footer>
+                <span>
+                {wordCount} words. {minutesToRead} min read
+                </span>
+            </footer>
+        </div>
     )
 }
 
