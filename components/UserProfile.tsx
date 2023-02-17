61ffec50146dd2bd1024247d8167ace79882bba6
JackTubby <jack200034@hotmail.com>
2023-02-17 17:52:39 +0000

    Create user profile showing basic user info

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,7 +1,14 @@
-const UserProfile = () => {
-    return (
-        <main>UserProfile</main>
-    )
-}
+// Pass user props & display user data
+const UserProfile = ({ user }) => {
+  return (
+    <div>
+      <img src={user.photoURL} alt="Users photo" />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName}</h1>
+    </div>
+  );
+};
 
 export default UserProfile;
